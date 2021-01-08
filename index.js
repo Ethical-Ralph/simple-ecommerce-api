c5e48084221eb22bc6c0c521df3da45f78a0bbc5
Akinola Raphael <54055273+Ethical-Ralph@users.noreply.github.com>
2021-01-08 08:24:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.get("/", (req,res) =>
 )
 
 app.post("/convert", (req,res) => {
-//  const { csv: { url, select_fields }} = req.body
+ const { csv: { url, select_fields }} = req.body
   try {
-     const result = convert()
+     const result = convert(url)
       res.json(result)
    } 
 catch(e) {
