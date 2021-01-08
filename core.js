95e7008faf51d6c298379aec3f078090ddecad7d
Akinola Raphael <54055273+Ethical-Ralph@users.noreply.github.com>
2021-01-08 08:02:29 +0100

    Create core.js

---
+++ core.js
@@ -0,0 +1,90 @@
+const http = require("http");
+const https = require("https");
+
+const getFile = (url, cb) => {
+  var response_data = "";
+  if(url.startWith("https://") {
+  https
+    .get("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv", (res) => {
+     console.log(res.headers)
+ res.on("data", function (data) {
+        response_data += data;
+      });
+      res.on("end", function () {
+        parse(response_data);
+      });
+    })
+    .on("error", (e) => {
+      throw new Error(e)
+    });
+    } else {
+    http
+    .get("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv", (res) => {
+      res.on("data", function (data) {
+        response_data += data;
+      });
+      res.on("end", function () {
+        parse(response_data);
+      });
+    })
+    .on("error", (e) => {
+      throw new Error(e)
+    });
+   }
+};
+
+const vParser = (csv) => {
+  var array = csv.split("\n");
+
+  let result = [];
+
+  let headers = array[0].split(", ");
+
+  for (let i = 1; i < array.length - 1; i++) {
+    let obj = {};
+    let str = array[i];
+    let s = "";
+
+    let flag = 0;
+    for (let ch of str) {
+      if (ch === '"' && flag === 0) {
+        flag = 1;
+      } else if (ch === '"' && flag == 1) flag = 0;
+      if (ch !== '"') s += ch;
+    }
+
+    let properties = s.split(",");
+
+    for (let j in headers) {
+      if (properties[j] && properties[j].includes(", ")) {
+        obj[headers[j]] = properties[j]
+          .split(/^"(\s)+$/g)
+          .map((item) => item.trim());
+      } else obj[headers[j]] = properties[j];
+    }
+    result.push(obj);
+  }
+
+  return result;
+};
+
+const parse = (data) => {
+  // const selectedF = ["LonD", "City", "State"];
+
+  const selectedF = ["LonD", "City", "State"];
+  const json = vParser(data);
+
+  console.log(
+    json.map((val) => {
+      const newData = {};
+      selectedF.forEach((field) => {
+        newData[field] = val[field];
+      });
+      return newData;
+    })
+  );
+
+  return json
+};
+
+module.export = getFile;
